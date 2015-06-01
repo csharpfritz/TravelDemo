d2393ed4c3840e754337cc562b62495b905ff84f
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2015-06-01 17:18:45 -0400

    Generated source

---
+++ TravelDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelDemo.Startup))]
+namespace TravelDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
